cfacdddba58ff99ec39d6e5740edf74c2e6a1717
Drew Sabelhaus <apsabelhaus@berkeley.edu>
2017-03-18 16:08:30 -0700

    Reorganizing CV to be single-column only. This commit is a reasonable re-working of the education section.

--- bib2xhtml/make_html_bib.sh
+++ bib2xhtml/make_html_bib.sh
@@ -4,4 +4,4 @@
 echo "Update this bash script to take in name of file."
 DATESTRING=$(date +"%Y-%m-%d")
 
-perl bib2xhtml.pl -c -r -n Sabelhaus -s plain bib_files/my_publications_2016-06-24.bib output_html/mypubs_$DATESTRING.html
+perl bib2xhtml.pl -c -r -R -n Sabelhaus -s plain bib_files/my_publications_2017-03-18.bib output_html/mypubs_$DATESTRING.html
