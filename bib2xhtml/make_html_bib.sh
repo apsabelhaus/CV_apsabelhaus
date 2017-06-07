45b035d1b501d79b1f2d72318fbd0bd094ce4dd5
Drew Sabelhaus <apsabelhaus@berkeley.edu>
2017-06-07 10:20:00 -0700

    Updated the make_html_bib script to take in arguments instead of needing to hard-code the path names. Should be easier to generate my HTML bibliographies now!

--- bib2xhtml/make_html_bib.sh
+++ bib2xhtml/make_html_bib.sh
@@ -1,7 +1,51 @@
 #!/bin/bash
 # a script to run bib2xhtml more easily.
 
-echo "Update this bash script to take in name of file."
-DATESTRING=$(date +"%Y-%m-%d")
+# First, get a list of options, and change output if needed.
+# Set the HELP variable to zero to start with. E.g., default to not outputing help.
+HELP=0
 
-perl bib2xhtml.pl -c -r -R -n Sabelhaus -s plain bib_files/my_publications_2017-03-18.bib output_html/mypubs_$DATESTRING.html
+# Declare the help message, which will be output in two places:
+HELPMESSAGE=$'\nThe make_html_bib.sh script requires one option: the name of the bib file to convert. \nBy default, this assumes the bib file is under ./bib_files/. \n \nOptions: -h, print this help message.'
+
+# Prefix for the file path:
+FILE_PREFIX='bib_files/'
+
+while getopts h option
+do
+    case "${option}"
+    in
+	h) HELP=1;;
+    esac
+done
+
+# If help is specified, print a message and exit.
+if [ "$HELP" == 1 ]; then
+    echo "$HELPMESSAGE"
+    echo "Exiting without creating an html file..."
+    exit
+fi
+
+# Otherwise, check on the number of arguments. Should only be one, if have not exited yet.
+if [ $# -eq 0 ]
+then
+    echo "$HELPMESSAGE"
+    echo "Exiting without creating an html file..."
+    exit
+elif [ $# -eq 1 ]
+then
+    # Actually create the file.
+    DATESTRING=$(date +"%Y-%m-%d")
+    FILE_PATH=./$FILE_PREFIX$1
+    FILE_OUTPUT=./output_html/mypubs_$DATESTRING.html
+    echo "Creating HTML file from" $FILE_PATH
+    perl bib2xhtml.pl -c -r -R -n Sabelhaus -s plain $FILE_PATH $FILE_OUTPUT
+    echo "Wrote" $FILE_OUTPUT
+    exit
+elif [ $# > 2 ]
+then
+    echo "Script only takes one argument. For help, pass in -h"
+    echo "Exiting without creating an html file..."
+    exit
+fi
+
